2a803914d4795efaa43d3efda88adb7f2902abd2
Tomdssdasd <3279333686@qq.com>
2019-02-21 19:04:00 +0800

    周考

--- Qust/Q5.py
+++ Qust/Q5.py
@@ -1,4 +1,4 @@
-
+#13700
 s="The column above illustrates apparently the polularity of people doing exercise in a certain year from 2013 to 2018 Based upon the data we can see that python is wonderful python is wonderful Python"
 result={}
 for i in  set(s):
